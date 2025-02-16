a668fda5d16c896250e7d789d7be2f9d409689d9
giorgi-gorjeladze <giorgigorjeladze21@gmail.com>
2025-02-16 23:20:30 +0400

    commit

---
+++ cw1.py
@@ -0,0 +1,7 @@
+1
+
+
+num7 = input()
+num8 = input()
+num9 = 1
+print(int(str(num7) + str(num8)) + num9)
---
+++ lesson2.py
@@ -0,0 +1,19 @@
+# კომპაილერი
+
+# data types
+#str
+#int
+#float 
+#bollian
+#none
+
+
+print("hello")
+print( 3 + 3)
+ 
+"gamarjoba"
+print("elemnti" * 10)
+print(100 + 900)
+print("hello" + "world")
+10 > 5 == True
+5 > 10 == False
