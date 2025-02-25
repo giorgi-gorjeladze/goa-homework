a89f08b05ad23fe1953f7bddea2ba4e36ef94be6
giorgi-gorjeladze <giorgigorjeladze21@gmail.com>
2025-02-25 18:01:50 +0400

    commit

---
+++ cw4.py
@@ -0,0 +1,17 @@
+for _ in range(3,25,2):
+    print(_)
+
+
+
+name = input()
+for i in name:
+    print(i)
+
+
+correct_password = "GOA"
+password = input()
+while correct_password != password:
+    password = input("wrong password")
+print("correct")
+    
+    
